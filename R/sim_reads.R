2c9181cb4f6169ee57002df2fc39cb6e022b2e4c
pblischak <blischak.4@osu.edu>
2015-06-19 11:05:47 -0400

    updated documentation for sim_reads script

--- R/sim_reads.R
+++ R/sim_reads.R
@@ -1,12 +1,15 @@
 #' Simulation of sequencing read counts and genotypes
 #'
-#' @description Simulates read counts for under the model of Blischak et al. (2015).
+#' @description Simulates genotypes and read counts under the model of Blischak et al. (2015).
 #' @param pVec A vector of allele frequencies strung together using the concatenate function.
 #' @param N_ind The number of individuals to simulate
 #' @param coverage The average number of sequenced simulated per individual per locus (Poisson distributed)
 #' @param ploidy The ploidy level of individuals in the population.
 #' @param error The probability of a sequencing error.
 #'
+#' @return A list of 3 matrices: (1) \code{genos} - a matrix of the simulated genotypes, (2) \code{tot_read_mat} - a matrix of the simulated number of total reads, (3) \code{ref_read_mat} - a matrix of the simulated number of reference reads.
+#'
+#' @references Blischak, P. D., L. S. Kubatko and A. D. Wolfe. 2015. Accounting for genotype uncertainty in the estimation of allele frequencies in autopolyploids. \emph{In review}. bioRxiv doi:####.
 #' @useDynLib polyfreqs
 #' @importFrom Rcpp sourceCpp
 
