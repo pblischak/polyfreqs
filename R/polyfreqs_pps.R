608ac621be98e142c4131c6bf4ddda3f48b6ad4d
pblischak <blischak.4@osu.edu>
2015-06-19 14:09:40 -0400

    got polyfreqs_pps working using r/t ratio

--- R/polyfreqs_pps.R
+++ R/polyfreqs_pps.R
@@ -14,16 +14,23 @@
 #' @export
 polyfreqs_pps <- function(p_post, tM, rM, ploidy, error){
 
-  ref_read_liks <- matrix(NA,nrow=nrow(p_post),ncol=ncol(p_post))
-  pred_genos <- matrix(apply(as.matrix(i), 1, function(x) rbinom(nrow(tM), ploidy, x)), nrow=nrow(tM), ncol=length(i))
-  obs_ref_read_probs <- ref_read_pmf(tM, rM, pred_genos, error)
-  obs_ref_read_lik <- apply(obs_ref_read_probs, 2, prod)
+  #sim_ref_read_liks <- matrix(NA, nrow=nrow(p_post), ncol=ncol(p_post))
+  sim_ref_read_ratios <- matrix(NA, nrow=nrow(p_post), ncol=ncol(p_post))
+  #obs_ref_read_liks <- matrix(NA,nrow=nrow(p_post),ncol=ncol(p_post))
+  obs_ref_read_ratio <- apply(rM/tM, 2, sum)
 
   for(i in 1:nrow(p_post)){
-    pred_genos <- matrix(apply(as.matrix(i), 1, function(x) rbinom(nrow(tM), ploidy, x)), nrow=nrow(tM), ncol=length(i))
-    pred_ref_reads <- sim_ref_reads(tM, pred_genos, ploidy, error)
-    ref_read_probs <- ref_read_pmf(tM, pred_ref_reads, pred_genos, error)
-
-    ref_read_liks[i,] <- apply(ref_read_probs, 2, prod)
+    sim_genos <- matrix(apply(p_post[i,], 2, function(x) rbinom(nrow(tM), ploidy, x)), nrow=nrow(tM), ncol=length(p_post[i,]))
+    sim_ref_read <- sim_ref_reads(tM, sim_genos, ploidy, error)
+    sim_ref_read_ratios[i,] <- apply(sim_ref_read/tM, 2, sum)
+    sim_ref_read_probs <- ref_read_pmf(tM, sim_ref_read, sim_genos, ploidy, error)
+    obs_ref_read_probs <- ref_read_pmf(tM, rM, sim_genos, ploidy, error)
+    #sim_ref_read_liks[i,] <- apply(log(sim_ref_read_probs), 2, sum)
+    #obs_ref_read_liks[i,] <- apply(log(obs_ref_read_probs), 2, sum)
   }
+
+  #likelihood_diff <- obs_ref_read_liks - sim_ref_read_liks
+  ratio_diff <- apply(sim_ref_read_ratios, 1, function(x) (obs_ref_read_ratio - x))
+  locus_vec <- apply(ratio_diff, 2, function(x) sum(sign(quantile(x,c(0.025,0.975))))==0)
+  return(list(ratio_diff=ratio_diff, locus_fit=locus_vec))
 }
